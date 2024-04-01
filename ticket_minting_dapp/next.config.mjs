0153694a759bce6bdecc516aca75948269c87894
Renzo Cabarios <renzo.cabarios@gmail.com>
2024-04-01 18:26:59 +0800

    Update nextConfig

--- ticket_minting_dapp/next.config.mjs
+++ ticket_minting_dapp/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gateway.irys.xyz",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
